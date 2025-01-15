1d47e10d493b890930eba9fbb18716a2ad45a83d
Chamath Heshan <chamathheshan076@gmail.com>
2025-01-15 20:52:59 +0530

    initial commit

---
+++ src/main/java/com/kafka/TrendingProductAnalyzer/KafkaTrendingProductAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.TrendingProductAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTrendingProductAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTrendingProductAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/TrendingProductAnalyzer/KafkaTrendingProductAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.TrendingProductAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTrendingProductAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
