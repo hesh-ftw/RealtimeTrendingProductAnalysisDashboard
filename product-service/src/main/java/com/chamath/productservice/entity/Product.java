bc030e79e93ff28914d7b49bd99c372399a38d24
Chamath Heshan <chamathheshan076@gmail.com>
2025-01-18 13:40:03 +0530

    add product entity

---
+++ product-service/src/main/java/com/chamath/productservice/entity/Product.java
@@ -0,0 +1,40 @@
+package com.chamath.productservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private Integer id;
+    private String description;
+    private Integer price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
